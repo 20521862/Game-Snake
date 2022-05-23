febb84e45647a1b0ae24a2b095c905efa42e4792
UIT-20521054 <104522978+UIT-20521054@users.noreply.github.com>
2022-05-23 14:15:54 +0700

    Thêm chức năng chọn tốc độ, tính giờ
    Sau mỗi 10s zone sẽ thu nhỏ lại 2 đơn vị

--- Snake.cpp
+++ Snake.cpp
@@ -1,10 +1,15 @@
 #include <windows.h>
 #include <iostream>
 #include <conio.h>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
 int Score = 0;
+int Speed = 180;
+int Time = 0;
+int Time2 = 10;
 
 struct NODE
 {
@@ -13,7 +18,7 @@ struct NODE
 
 struct SNAKE
 {
-    NODE A[100];
+    NODE A[1000];
     int Length;
 };
 
@@ -34,6 +39,38 @@ void gotoxy(int x, int y)
     pos.Y = y;
 
     SetConsoleCursorPosition(hConsole, pos);
+}
+
+void SetSpeed()
+{
+    cout << "\tSelect game speed:\n";
+    cout << "\t1. Slow\n";
+    cout << "\t2. Normal\n";
+    cout << "\t3. Fast\n";
+    int n;
+    do
+    {
+        cout << "Please choice 1, 2 or 3: ";
+        cin >> n;
+    } while (n < 1 || n > 3);
+    if (n == 1)
+    {
+        Speed = 230;
+    }
+    else if (n == 3)
+    {
+        Speed = 130;
+    }
+    system("cls");
+}
+
+void ThreadTime()
+{
+    while (true)
+    {
+        Time++;
+        Sleep(1000);
+    }
 }
 
 void InitZone(ZONE &zone)
@@ -71,6 +108,33 @@ void DrawZone(ZONE zone)
     }
 }
 
+void EraseZone(ZONE zone)
+{
+    gotoxy(zone.left, zone.top);
+    for (int i = zone.left; i < zone.right; i++)
+    {
+        cout << " ";
+    }
+
+    for (int i = zone.top; i < zone.bottom; i++)
+    {
+        gotoxy(zone.left, i);
+        cout << " ";
+    }
+
+    gotoxy(zone.left, zone.bottom);
+    for (int i = zone.left; i < zone.right; i++)
+    {
+        cout << " ";
+    }
+
+    for (int i = zone.top; i < zone.bottom; i++)
+    {
+        gotoxy(zone.right, i);
+        cout << " ";
+    }
+}
+
 void InitSnake(SNAKE &snake)
 {
     snake.Length = 4;
@@ -86,11 +150,17 @@ void InitSnake(SNAKE &snake)
 
 void DrawSnake(SNAKE snake)
 {
-    int i;
-    for (i = 0; i < snake.Length; i++)
+    for (int i = 0; i < snake.Length; i++)
     {
         gotoxy(snake.A[i].x, snake.A[i].y);
-        cout << "O"; // hinh dang con ran
+        if (i == 0)
+        {
+            cout << "O"; // hinh dang dau con ran
+        }
+        else
+        {
+            cout << "o"; // hinh dang than con ran
+        }
     }
 }
 
@@ -141,60 +211,102 @@ void Run(SNAKE &snake, int &dir, int olddir)
     }
 }
 
-void DrawScore(ZONE zone)
+int RandomInt(int min, int max)
 {
-    gotoxy(zone.right + 5, zone.top);
+    srand(time(NULL));
+    int n = rand() % (max - min + 1) + min;
+    return n;
+}
+
+NODE DrawFood(ZONE zone)
+{
+    NODE food;
+    food.x = RandomInt(zone.left + 1, zone.right - 1);
+    food.y = RandomInt(zone.top + 1, zone.bottom - 1);
+    gotoxy(food.x, food.y);
+    cout << "X"; // hinh dang thuc an
+    return food;
+}
+
+void EraseFood(NODE food)
+{
+    gotoxy(food.x, food.y);
+    cout << " ";
+}
+
+void DrawScore()
+{
+    gotoxy(40, 5);
     cout << "SCORE: " << Score;
+}
+
+void DrawTime()
+{
+    gotoxy(40, 10);
+    cout << "TIME: " << Time;
 }
 
 void GameOver()
 {
     system("cls");
     cout << "\n\n\n\t\tGAME OVER!\n\n";
-    cout << "\n\n\n\t\tSCORE: " << Score << "\n";
-    cout << "\n\n\n\t\tGoodbye! See you again!";
-}
-
-int RandomInt(int min, int max)
-{
-    int n = rand() % (max - min + 1) + min;
-    return n;
+    cout << "\n\n\n\t\tSCORE: " << Score << "\n\n";
+    cout << "\n\n\n\t\tGoodbye! See you again!\n\n";
 }
 
 int main()
 {
+    SetSpeed();
+
     SNAKE snake;
     NODE food;
     ZONE zone;
     InitZone(zone);
     DrawZone(zone);
 
+    thread time(ThreadTime);
+    time.detach();
+
     int dir = 0;
     int olddir = 0;
     InitSnake(snake);
     DrawSnake(snake);
 
-    food.x = RandomInt(zone.left + 1, zone.right - 1); // Random vi tri thuc an
-    food.y = RandomInt(zone.top + 1, zone.bottom - 1);
-    
-    gotoxy(food.x, food.y);
-    cout << "X"; // hinh dang thuc an
+    food = DrawFood(zone);
 
     while (1)
     {
-        gotoxy(snake.A[snake.Length - 1].x, snake.A[snake.Length - 1].y); // xoa duoi
+        if (Time == Time2)
+        {
+            Time2 += 10;
+            EraseZone(zone);
+            zone.bottom -= 2;
+            zone.left += 2;
+            zone.right -= 2;
+            zone.top += 2;
+            EraseFood(food);
+            DrawZone(zone);
+            food = DrawFood(zone);
+        }
+
+        gotoxy(snake.A[snake.Length - 1].x, snake.A[snake.Length - 1].y); // xóa đuôi
         cout << " ";
 
         for (int i = 0; i < snake.Length; i++)
         {
-            if (snake.A[i].x == food.x && snake.A[i].y == food.y) // neu dau ran cham thuc an
+            if (snake.A[i].x == food.x && snake.A[i].y == food.y) // Nếu thân rắn chạm thức ăn
             {
-                snake.Length++;
-                Score++;
-                food.x = RandomInt(zone.left + 1, zone.right - 1);
-                food.y = RandomInt(zone.top + 1, zone.bottom - 1);
-                gotoxy(food.x, food.y);
-                cout << "X";
+                if (i == 0) // Chỉ đầu rắn chạm thức ăn mới được điểm
+                {
+                    snake.Length++;
+                    Score++;
+                }
+                else // Nếu lỡ food có random trong phần thân rắn
+                {
+                    gotoxy(food.x, food.y);
+                    cout << " ";
+                }
+                food = DrawFood(zone);
             }
         }
 
@@ -211,11 +323,12 @@ int main()
             else if (c == 'w') // di len
                 dir = 3;
         }
+
         Run(snake, dir, olddir);
         DrawSnake(snake);
-        DrawScore(zone);
-        gotoxy(0, 0);
-        cout << " ";
+        DrawScore();
+        DrawTime();
+        gotoxy(0, 0); // Đưa con trỏ lên góc trên bên trái
 
         int flag = 0;
         for (int i = 1; i < snake.Length; i++)
@@ -226,7 +339,7 @@ int main()
                 break;
             }
         }
-        if (snake.A[0].x <= zone.left || snake.A[0].y <= zone.top || snake.A[0].x >= zone.right || snake.A[0].y >= zone.bottom)
+        if (snake.A[0].x <= zone.left || snake.A[0].y <= zone.top || snake.A[0].x >= zone.right || snake.A[0].y >= zone.bottom) // Nếu chạm vào tường
         {
             flag = 1;
         }
@@ -234,7 +347,7 @@ int main()
         {
             break;
         }
-        Sleep(180);
+        Sleep(Speed);
     }
     GameOver();
     system("pause");
